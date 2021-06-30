75ebbf461dfb9dc8a6b6828c1487ded0b995fa06
JhoRodrigo <jrodrigomdc@hotmail.com>
2021-06-29 22:30:53 -0300

    Commit Inicial

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")] // Função para aplicar o acento na variavel
+        [Required(ErrorMessage = "O campo descrição é obrigatório")] // Para trazer uma mensagem em baixo do textbox
+        public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
